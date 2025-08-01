f09e9442574a757d976138f651cafea8854c4880
JovanJevtic <ovojovanovo@gmail.com>
2025-08-01 02:33:07 +0200

    update to longi

--- packages/core/admin/admin/src/pages/Settings/pages/Roles/utils/updateConditionsToFalse.ts
+++ packages/core/admin/admin/src/pages/Settings/pages/Roles/utils/updateConditionsToFalse.ts
@@ -7,9 +7,8 @@ import { createArrayOfValues } from './createArrayOfValues';
 /**
  * Changes all the conditions leaf when the properties are all falsy
  */
-const updateConditionsToFalse = (obj: object): object => {
-  return Object.keys(obj).reduce((acc, current) => {
-    // @ts-expect-error – TODO: type better
+const updateConditionsToFalse = (obj: Record<string, any>): object => {
+  return Object.keys(obj).reduce((acc: Record<string, any>, current) => {
     const currentValue = obj[current];
 
     if (isObject(currentValue) && !has(currentValue, 'conditions')) {
@@ -22,19 +21,19 @@ const updateConditionsToFalse = (obj: object): object => {
       );
 
       if (!isActionEnabled) {
-        // @ts-expect-error – TODO: type better
-        const updatedConditions = Object.keys(currentValue.conditions).reduce((acc1, current) => {
-          // @ts-expect-error – TODO: type better
-          acc1[current] = false;
+        const updatedConditions = Object.keys(currentValue.conditions).reduce(
+          (acc1: Record<string, boolean>, current) => {
+            acc1[current] = false;
 
-          return acc1;
-        }, {});
+            return acc1;
+          },
+          {}
+        );
 
         return { ...acc, [current]: { ...currentValue, conditions: updatedConditions } };
       }
     }
 
-    // @ts-expect-error – TODO: type better
     acc[current] = currentValue;
 
     return acc;
